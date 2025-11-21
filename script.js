3c3368af7c1a91682aec92132b236d9fc982dabf
Enyholla <sannifawaz2504@gmail.com>
2025-11-21 01:28:44 +0100

    Implement the highscore, done with the game

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 
 let score = 20;
+
+let highScore = 0;
 //Add event listener to the check button that grabs a value from the input field whenever its clicked 
 document.querySelector(".check").addEventListener("click", function () {
     const guess = Number(document.querySelector(".guess").value);
@@ -15,6 +17,11 @@ document.querySelector(".check").addEventListener("click", function () {
         document.querySelector(".number").textContent = secretNumber;
         document.querySelector(".number").style.width = '30rem';
         document.querySelector("body").style.backgroundColor = '#60b347';
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector(".highscore").textContent = highScore;
+        }
+
     }else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector(".message").textContent = "📈 too high";
